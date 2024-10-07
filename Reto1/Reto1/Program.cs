ea522f1cc8c120cb744cf4c3ea3bdc9034993c1c
ivanzerpaz.22@gmail.com <ivanzerpaz.22@gmail.com>
2024-10-06 21:10:51 -0400

    EJERCICIO 1

---
+++ Reto1/Reto1/Program.cs
@@ -0,0 +1,32 @@
+﻿
+using System;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 100; i++)
+            {
+                if (i % 3 == 0 && i % 5 == 0)
+                {
+                    Console.WriteLine("FizzBuz ");
+                }
+                else if (i % 3 == 0)
+                {
+                    Console.WriteLine("Fizz");
+
+                }
+                else if (i % 5 == 0)
+                {
+                    Console.WriteLine("Buzz");
+                }
+                else
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
